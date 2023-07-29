aec861e683bb03d675dede10e2060dcfe9627806
Lakshya S <slakshya826@gmail.com>
2023-07-29 12:00:10 +0530

    insert data to mongodb

--- insert.js
+++ insert.js
@@ -1,1 +1,14 @@
 const dbConnect = require('./mongodb')
+
+const insert = async () => {
+    const db = await dbConnect();
+    const result =await db.insertMany(
+        [
+            {name: 'Lakshya',brand: 'Sharma',price: 350,category: 'Mobile'},
+            {name: 'Lsya',brand: 'Hello',price: 250,category: 'Tablet'},
+            {name: 'Laksfdahya',brand: 'Kohli',price: 650,category: 'Laptop'}
+        ])  
+    console.log(result.acknowledged)
+}
+
+insert();
